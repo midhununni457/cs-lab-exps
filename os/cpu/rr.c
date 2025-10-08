a4d20b2beaf7db913312cb190cf3c5943a94aafc
Midhun Unni <96683120+midhununni457@users.noreply.github.com>
2025-10-08 20:32:56 +0530

    update rr.c

--- os/cpu/rr.c
+++ os/cpu/rr.c
@@ -1,72 +1,118 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct process {
-    int id;
-    int at;
-    int bt;
-    int ct;
-    int tat;
-    int wt;
-};
+typedef struct {
+    int name;
+    int arrival;
+    int burst;
+    int remaining;
+    int status;
+    int completion;
+    int waiting;
+    int turnaround;
+} Process;
 
-struct process queue[100];
-int front = 0, rear = -1;
+typedef struct Node {
+    Process process;
+    struct Node* next;
+} Node;
+Node *front=NULL;
+Node *rear=NULL;
 
-void main() {
-    int n;
-    printf("Enter no: of processes: ");
-    scanf("%d", &n);
-    struct process p[n];
-    for (int i=0; i<n; i++) {
-        int a, b;
-        printf("Enter arrival time of process %d: ", i+1);
-        scanf("%d", &a);
-        printf("Enter burst time of process %d: ", i+1);
-        scanf("%d", &b);
-        p[i].id = i+1;
-        p[i].at = a;
-        p[i].bt = b;
-        p[i].ct = 0;
-        p[i].tat = 0;
-        p[i].wt = 0;
+void enqueue(Process process) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+ 
+    newNode->process = process;
+    newNode->next = NULL;
+
+    if (front == NULL) {
+        front=rear = newNode;
+    } else {
+        rear->next = newNode;
+        rear = newNode;
     }
-    int tq, cur_time = 0, completed = 0;
-    printf("Enter time quantum: ");
-    scanf("%d", &tq);
-    while (completed < n) {
-        for (int i=0; i<n; i++) {
-            if (p[i].at <= cur_time && p[i].at > cur_time-tq) {
-                queue[++rear] = p[i];
+}
+
+Process dequeue() {
+   
+    Node* temp = front;
+    Process process = temp->process;
+    front = front->next;
+    free(temp);
+    return process;
+}
+void printqueue()
+{
+    Node *temp=front;
+    while(temp!=NULL)
+    {
+        printf("%d-->",temp->process.name);
+        temp=temp->next;
+        
+    }
+    printf("NULL\n");
+}
+int main() {
+    Process processes[] = {
+        {1, 0, 5, 5,0,-1,-1,-1},  
+        {2, 1, 6, 6,0,-1,-1,-1},  
+        {3, 2, 3, 3,0,-1,-1,-1},
+        {4, 3, 1, 1,0,-1,-1,-1},  
+        {5, 4, 5, 5,0,-1,-1,-1},  
+        {6, 6, 4, 4,0,-1,-1,-1}
+        
+    };
+    int n = sizeof(processes) / sizeof(Process);
+
+    int quantum = 4;
+
+    int currenttime = 0;
+    int completed = 0;
+   
+    printf("Time  |  Process\n");
+    printf("------+-------------------\n");
+
+     enqueue(processes[0]);
+     processes[0].status=1;
+    while (completed < n ) {
+        if (front != NULL) {
+            Process currentProcess = dequeue();
+            int execution_time = (currentProcess.remaining < quantum) ? currentProcess.remaining : quantum;
+            currentProcess.remaining -= execution_time;
+            currenttime += execution_time;
+            for (int i = 0; i < n; i++) {
+            if (processes[i].arrival <= currenttime && processes[i].status!=1) {
+                enqueue(processes[i]);
+                processes[i].status=1;
             }
         }
-        if (front > rear) {
-            cur_time++;
-            continue;
-        }
-        if (queue[front].bt <= tq) {
-            cur_time += queue[front].bt;
-            for (int i=0; i<n; i++) {
-                if (p[i].id == queue[front].id) {
-                    p[i].ct = cur_time;
-                    p[i].tat = p[i].ct - p[i].at;
-                    p[i].wt = p[i].tat - p[i].bt;
-                }
+            printf("%-5d |  P%d\n", currenttime, currentProcess.name);
+            if (currentProcess.remaining <= 0) {
+                completed++;
+                for(int i=0;i<n;i++) if (processes[i].name==currentProcess.name)
+                           {processes[i].completion=currenttime;break;}
+            } else {
+                currentProcess.status=1;
+                enqueue(currentProcess);
             }
-            completed++;
-            front++;
         } else {
-            cur_time += tq;
-            queue[++rear] = queue[front];
-            queue[rear].bt -= tq;
-            front++;
+            currenttime++;
         }
     }
-    int tot_tat = 0, tot_wt = 0;
-    printf("Process\tAT\tBT\tCT\tTAT\tWT\n");
-    for (int i=0; i<n; i++) {
-        tot_tat += p[i].tat;
-        tot_wt += p[i].wt;
-        printf("%d\t%d\t%d\t%d\t%d\t%d\n", i+1, p[i].at, p[i].bt, p[i].ct, p[i].tat, p[i].wt);
+
+    float avwt=0.0;
+    float avtat=0.0;
+    printf("Process ArrivalTime BurstTime  CompletionTime  TurnAroundTime WaitingTime\n");
+    for(int i=0;i<n;i++)
+    {
+    processes[i].turnaround=processes[i].completion-processes[i].arrival;
+    processes[i].waiting=processes[i].turnaround-processes[i].burst;
+    printf("P%d%10d%10d%20d%10d%20d\n",processes[i].name,processes[i].arrival,processes[i].burst,processes[i].completion,processes[i].turnaround,processes[i].waiting);
+    avwt=avwt+processes[i].waiting;
+    avtat=avtat+processes[i].turnaround;
     }
-    printf("Avg TAT: %.2f\nAvg WT: %.2f\n", (float)tot_tat/n, (float)tot_wt/n);
+    printf("Average Waiting Time=%f\n",avwt/n);
+    printf("Average Turn Around Time=%f\n",avtat/n);
+      return 0;  
 }
